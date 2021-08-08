d145e5626d6232be4398a43a9a65d2518e77d35a
snoski <jeff.b.carter@gmail.com>
2021-08-08 06:45:33 -0500

    lots of changes

--- behavior.js
+++ behavior.js
@@ -31,6 +31,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			var imageSet = elemID.substr(elemID.length -1,1);
 			var oldMonth = monthNums[imageSet - 1];
 			var newMonth = incrMonth(incr,imageSet);
+			console.log("old month = ",oldMonth);
+			console.log("new month = ",newMonth);
 			document.getElementById("img_"+imageSet+"-"+oldMonth).style.display = "none";
 			document.getElementById("img_"+imageSet+"-"+newMonth).style.display = "inline";
 		});
@@ -41,15 +43,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		histograms[i].addEventListener("click", function () {
 			var elemID = this.id;
 			var imageSet = elemID.substr(4,1);
-			if (allMonths[imageSet]) {
-				allMonths[imageSet] = false;
+			if (allMonths[imageSet - 1]) {
+				allMonths[imageSet - 1] = false;
 				this.style.display = "none";
 				var month = monthNums[imageSet - 1];
 				document.getElementById("img_" + imageSet + "-" + month).style.display = "inline";
 				document.getElementById("prev-arrow-" + imageSet).style.display = "inline";
 				document.getElementById("next-arrow-" + imageSet).style.display = "inline";
 			} else {
-				allMonths[imageSet] = true;
+				allMonths[imageSet - 1] = true;
 				this.style.display = "none";
 				document.getElementById("img_" + imageSet + "-all").style.display = "inline";
 				document.getElementById("prev-arrow-" + imageSet).style.display = "none";
@@ -69,5 +71,47 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 		return monthNums[imageSet - 1];
 	}
+
+	//CODE FOR CUMULATIVE PERCENTAGE CHART
+	var cumul = document.getElementById("cumul-1");
+	cumul.addEventListener("click", function() {
+		this.style.display = "none";
+		document.getElementById("cumul-2").style.display = "inline";
+		document.getElementById("admonishment").style.display = "none";
+		document.getElementById("cumul-p").style.display = "block";
+	});
+
+	//CODE FOR MEAN TOGGLE
+	var cumul = document.getElementById("mean-toggle");
+	cumul.addEventListener("click", function() {	
+		if (this.innerHTML === 'Toggle Mean On') {
+			this.innerHTML = 'Toggle Mean Off';
+			oldImageSet = 3;
+			imageSet = 4;					
+			monthNums[3] = monthNums[2];
+			allMonths[3] = allMonths[2];
+		} else {
+			this.innerHTML = 'Toggle Mean On';
+			oldImageSet = 4;
+			imageSet = 3;				
+			monthNums[2] = monthNums[3];
+			allMonths[2] = allMonths[3];			
+		}
+		var month = monthNums[imageSet - 1];
+		console.log("allMonths",allMonths[imageSet - 1]);
+		if (allMonths[imageSet - 1]) {
+			document.getElementById("img_" + oldImageSet + "-all").style.display = "none";
+			document.getElementById("img_" + imageSet + "-all").style.display = "inline";
+			document.getElementById("prev-arrow-" + imageSet).style.display = "none";
+			document.getElementById("next-arrow-" + imageSet).style.display = "none";
+		} else {
+			document.getElementById("img_" + oldImageSet + "-" + month).style.display = "none";
+			document.getElementById("prev-arrow-" + oldImageSet).style.display = "none";
+			document.getElementById("next-arrow-" + oldImageSet).style.display = "none";
+			document.getElementById("img_" + imageSet + "-" + month).style.display = "inline";
+			document.getElementById("prev-arrow-" + imageSet).style.display = "inline";
+			document.getElementById("next-arrow-" + imageSet).style.display = "inline";
+		}
+	});
 
 });
